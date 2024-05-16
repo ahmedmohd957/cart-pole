3516e845691f373d297a83da11395f62a19d0f13
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-05-16 20:18:54 +0200

    refactor: :recycle: Refactor q-learning code

---
+++ q_learning/run.py
@@ -0,0 +1,38 @@
+import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
+from q_learning import Q_Learning
+
+env = gym.make('CartPole-v1')
+
+config = {
+    'env': env,
+    'episodes': 350,
+    'epsilon': float,
+    'min_epsilon': 0.005,
+    'gamma': 0.95,
+    'learning_rate': 0.5,
+    'min_learning_rate': 0.01,
+    'seed': 1,
+    'bins': (1,1,6,3),
+    'num_action_space': env.action_space.n
+}
+
+q = Q_Learning(config)
+episode_rewards, average_reward, convergence_episode, training_time = q.train()
+
+# Print metrics
+print(f"Average Cumulative Reward (last 100): {np.mean(episode_rewards[-100:])}")
+print(f"Convergence Episode: {convergence_episode}")
+print(f"Training Time (seconds): {training_time}")
+
+plt.figure()
+plt.plot(range(config['episodes']), episode_rewards, label = "Reward")
+plt.plot(range(config['episodes']), average_reward, label = "Average Reward")
+plt.axhline(y=475, color='r', linestyle='--', label='475 Steps')
+plt.legend(loc='upper left')
+plt.title(f"Q-learning Training Result")
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.savefig(f'Q-learning.png')
+plt.show()
